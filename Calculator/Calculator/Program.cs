285aaf6385342f3e7cdafce9fd217ebd1c193dc1
Ni1py <gekepyrkov@gmail.com>
2021-03-23 04:45:19 +0300

    calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Calculate();
+        }
+		public static void Calculate()
+		{
+			Console.Write("Enter first number: ");
+			string number1 = Console.ReadLine();
+			Console.Write("Enter second number: ");
+			string number2 = Console.ReadLine();
+			Console.Write("Enter the calculation operation: ");
+			string operation = Console.ReadLine();
+			int calcul = 0;
+			if (operation == "+")
+			{
+				calcul = Convert.ToInt32(number1) + Convert.ToInt32(number2);
+			}
+			if (operation == "-")
+			{
+				calcul = Convert.ToInt32(number1) - Convert.ToInt32(number2);
+			}
+			if (operation == "*")
+			{
+				calcul = Convert.ToInt32(number1) * Convert.ToInt32(number2);
+			}
+			if (operation == "/")
+			{
+				calcul = Convert.ToInt32(number1) / Convert.ToInt32(number2);
+			}
+			Console.WriteLine($"{number1} {operation} {number2} = {calcul}");
+			}
+	}
+}
