1be9c8dbe6bb05a978abe8cd67a9cb4e7ccbe65f
Ni1py <gekepyrkov@gmail.com>
2021-04-15 05:58:51 +0300

    добавил 6 задание

---
+++ StatisticsSQL/StatisticsSQL/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StatisticsSQL
+{
+    class Program
+    {
+        private static string _connectionString = @"Server=DESKTOP-3KKRO48\SQLEXPRESS01;Database=CustomerOrder;Trusted_Connection=True;";
+
+        static void Main(string[] args)
+        {
+            List<Statistics> orderStatistics = ReadPosts();
+            int increment = 1;
+            Console.WriteLine("Statistics: ");
+            foreach (Statistics record in orderStatistics)
+            {
+                Console.WriteLine();
+                Console.Write($"Name: {record.Name};");
+                if ((increment == 2) || (increment == 3)) { Console.Write("\t"); }
+                Console.Write($"\t Number of products: {record.NumberOfProducts};");
+                Console.WriteLine($"\t\t Order amount: {record.OrderAmount}.");
+                increment++;
+            }
+            Console.ReadKey();
+        }
+
+        private static List<Statistics> ReadPosts()
+        {
+            List<Statistics> orderStatistics = new List<Statistics>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText =
+                        @"SELECT 
+                            [Name], 
+                            COUNT([Order].[CustomerId]) AS [Number of products],
+                            SUM([Price]) AS [Order amount]
+                          FROM 
+                            Customer
+                          JOIN [Order]
+                            ON [Order].[CustomerId] =  [Customer].[CustomerId]
+                          GROUP BY [Name];";
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var record = new Statistics
+                            {
+                                Name = Convert.ToString(reader[0]),
+                                NumberOfProducts = Convert.ToInt32(reader[1]),
+                                OrderAmount = Convert.ToInt32(reader[2]),
+                            };
+                            orderStatistics.Add(record);
+                        }
+                    }
+                }
+            }
+            return orderStatistics;
+        }
+    }
+}
---
+++ StatisticsSQL/StatisticsSQL/Statistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticsSQL
+{
+    class Statistics
+    {
+        public string Name;
+        public int NumberOfProducts;
+        public int OrderAmount;
+    }
+}
